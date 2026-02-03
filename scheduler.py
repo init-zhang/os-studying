8bd7418c001084678aafc820e0d14b856a727eeb
init-zhang <init-zhang@proton.me>
2026-02-03 09:06:52 +0000

    Remove stack and heap

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,3 @@
-from cpu_registers import CPURegisters
-from pcb import ProcessControlBlock
-
-class Scheduler:
-    def __init__(self, cpu, memory, blah):
-        self.cpu = cpu
-        self.memory = memory
-        self.blah = blah
-
-    def load_process(self, process):
-
-STACK_MAX_INDEX = 8
-HEAP_MAX_INDEX = 32
 PROCESS_LIST_MAX_INDEX = 64
 # Process size = 8,16,32 = 56 spaces
 PROCESS_SIZE = 56
@@ -18,22 +5,11 @@ PROCESS_CONTEXT_SIZE = 8
 PROCESS_DATA_SIZE = 16
 PROCESS_CODE_SIZE = 32
 
-memory = [0] * 1024  # 64-bit integer -> 8 bytes, 1024 of 8 bytes
-stack_index = 0
-heap_index = STACK_MAX_INDEX
-process_list_index = HEAP_MAX_INDEX
-process_index = PROCESS_LIST_MAX_INDEX
 
-def append_to_stack(memory, value):
-    memory[stack_index] = value
-    stack_index += 1
-
-def top_from_stack(memory):
-    return memory[stack_index-1]
-
-def pop_from_stack(memory):
-    stack_index -= 1
-    return memory[stack_index]
+# Memory
+memory = [0] * 1024
+# CPU registers
+cpu = [0] * 16
 
 def add_process(memory, context, data, code):
     # Write process index to process index list
