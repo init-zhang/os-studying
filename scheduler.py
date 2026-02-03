00fe6d06678b032b6c7371605dd0bd57a08664d8
init-zhang <init-zhang@proton.me>
2026-02-03 19:25:21 +0000

    Shrink OS memory to 32 words

--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,19 @@
 # Memory
-# 0-22 OS and scheduler
-# 23 Current PID
-# 24 Queue tail
-# 25 Queue head
-# 26-31 Process queue
-# 32 Next free PID
-# 33-63 Process list
-# 64-1023 Processes
-M_CURRENT_PID = 23
-M_QUEUE_TAIL = 24
-M_QUEUE_HEAD = 25
-M_QUEUE_BASE = 26
-M_QUEUE_END = 31
-M_PROCESS_LIST = 32
-M_PROCESSES = 64
+# 0-31 OS and scheduler
+# 15 Current PID
+# 16 Queue tail
+# 17 Queue head
+# 18-23 Process queue
+# 24 Next free PID
+# 25-31 Process list
+# 32-1023 Processes
+M_CURRENT_PID = 15
+M_QUEUE_TAIL = 16
+M_QUEUE_HEAD = 17
+M_QUEUE_BASE = 18
+M_QUEUE_END = 23
+M_PROCESS_LIST = 24
+M_PROCESSES = 32
 def init_memory():
     memory = [0xFFFF] * 256
     memory[M_PROCESS_LIST] = 0
