a67eaa88408fd86b1b510daf8d6f91c28fcc9336
init-zhang <init-zhang@proton.me>
2026-02-12 18:40:52 +0000

    Move memory constants to constants.py

--- memory.py
+++ memory.py
@@ -1,19 +1,4 @@
-# Memory
-# 0-31 OS and scheduler
-# 15 Current PID
-# 16 Queue tail
-# 17 Queue head
-# 18-23 Process queue
-# 24 Next free PID
-# 25-31 Process list
-# 32-1023 Processes
-M_CURRENT_PID = 15
-M_QUEUE_TAIL = 16
-M_QUEUE_HEAD = 17
-M_QUEUE_BASE = 18
-M_QUEUE_END = 23
-M_PROCESS_LIST = 24
-M_PROCESSES = 32
+from constants import *
 
 def init_memory():
     memory = [0xFFFF] * 256
